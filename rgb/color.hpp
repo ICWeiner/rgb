018858d21cfe7e6b67708f110f2f6173fde0436a
ICWeiner <diogoffn@gmail.com>
2021-12-20 11:17:28 -0800

    added doxygen to color

--- rgb/color.hpp
+++ rgb/color.hpp
@@ -5,34 +5,39 @@
 #include <iostream>
 
 namespace rgb {
+    //!Custom data type for storing color values between 0 and 255
     typedef unsigned char rgb_value;
 
+    //! Class that represents a single pixel
     class color {
     private:
-        rgb_value r;
-        rgb_value g;
-        rgb_value b;
+        rgb_value r;//! Red value
+        rgb_value g;//! Green value
+        rgb_value b;//! Blue value
     public:
-        static const color RED;
-        static const color GREEN;
-        static const color BLUE;
-        static const color BLACK;
-        static const color WHITE;
-        color();
-        color(const color &c);
-        color(rgb_value r, rgb_value g, rgb_value b);
-        rgb_value red() const;
-        rgb_value& red();
-        rgb_value green() const;
-        rgb_value& green();
-        rgb_value blue() const;
-        rgb_value& blue();
-        color& operator=(const color&c);
-        bool operator==(const color &c) const;
-        bool operator!=(const color &c) const;
-        void invert();
-        void to_gray_scale();
-        void mix(const color& c, int f);
+        static const color RED;  //!Sets this pixel to the color red
+        static const color GREEN;//!Sets this pixel to the color green
+        static const color BLUE;//!Sets this pixel to the color blue
+        static const color BLACK;//!Sets this pixel to the color black
+        static const color WHITE;//!Sets this pixel to the color white
+        color();//! Empty constructor
+        color(const color &c);//! Copy constructor
+        color(rgb_value r, rgb_value g, rgb_value b);//! Manual color constructor
+        rgb_value red() const; //!returns red as a constant
+        rgb_value& red();//!returns red by reference
+        rgb_value green() const;//!returns green as a constant
+        rgb_value& green();//!returns green by reference
+        rgb_value blue() const;//!returns blue as a constant
+        rgb_value& blue();//!returns blue by reference
+        color& operator=(const color&c);//! assignment overload
+        bool operator==(const color &c) const;//! equals overload
+        bool operator!=(const color &c) const;//! different than overload
+        void invert();//! inverts color of pixel
+        void to_gray_scale();//! sets pixel to gray scale
+        void mix(const color& c, int f);/*! Mixes color of pixel with outbound pixel by a factor of f
+        * \param color outbound pixel
+        * \param f factor of mix, between 0 and 100
+        */
     };
 }
 #endif
